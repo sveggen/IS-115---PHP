b190b7e612e60eef637ad4a7d12b8771092cc145
Markus Sveggen <marks18@uia.no>
2020-08-20 19:48:11 +0200

    endret struktur

---
+++ public/modul-1/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="/style.css">
+</head>
+    <body>
+        <h2>Velkommen til Norge.no</h2>
+        <?php
+        echo "<p>Hovedstaden i Norge er: ";
+        echo "<strong>Bergen</strong>";
+        echo "<br />";
+        ?>
+
+        I dag er det den <?php echo date("d.m.Y");?>
+        </p>
+    </body>
+</html>
