1596e14ecf4d02161651f5fb63e2bcac78cf5b09
Markus Sveggen <marks18@uia.no>
2020-08-26 00:34:07 +0200

    ferdigstilte modul 1

--- public/modul-1/index.php
+++ public/modul-1/index.php
@@ -6,12 +6,19 @@
     <body>
         <h2>Velkommen til Norge.no</h2>
         <?php
-        echo "<p>Hovedstaden i Norge er: ";
-        echo "<strong>Bergen</strong>";
-        echo "<br />";
+        echo "<p>Hva er hovedstaden i Norge? ";
+        echo "<strong>Oslo</strong>";
+        echo "<br/>";
         ?>
-
-        I dag er det den <?php echo date("d.m.Y");?>
+        I dag er det den <b><?php echo date("d.m.Y");?></b>
         </p>
+        <?php
+        //Mine endringer
+    $syttendemai = strtotime('17.05.2021');
+    $idag = strtotime(date("d.m.Y"));
+    $tidigjen = $syttendemai - $idag;
+    $dagerigjen = abs(round($tidigjen / 86400)); //86400 = antall sek. i en dag
+    echo "I dag er det <b>$dagerigjen dager</b> igjen til Norges nasjonaldag 17. mai</p>";
+    ?>  
     </body>
-</html>
+</html>     
