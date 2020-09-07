5249065548dd4e4a8ebedfd31d47a63f67b78bee
Markus Sveggen <marks18@uia.no>
2020-09-07 23:03:26 +0200

    modul 3

---
+++ public/modul-3/teller.php
@@ -0,0 +1,18 @@
+<!-- Oppgave 2 -->
+<html>
+    <head>
+        <title>Teller</title>
+
+    </head>
+<body>
+<?php
+for($tall=0; $tall < 10; $tall++){
+    sleep(1); 
+    echo $tall . "<br>";
+    $sum += $tall;
+}
+echo "<br> Juhuu, ferdig å telle! Summen av tallene ble $sum";
+
+?>
+</body>
+</html>
---
+++ public/modul-3/velkomsthilsen.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Velkomsthilsen</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="/modul-3/style.css">
+</head>
+<body>
+
+<?php
+$fornavn = "";
+
+
+?>
+
+
+
+
+
+</body>
+</html>
