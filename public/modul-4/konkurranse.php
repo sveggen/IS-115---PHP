73021402a1c084bbc2de8d6c3c7a7ab927028450
Markus Sveggen <marks18@uia.no>
2020-09-13 19:22:26 +0200

    gjorde de første oppgavene i modul 4

--- public/modul-4/konkurranse.php
+++ public/modul-4/konkurranse.php
@@ -1,1 +1,44 @@
 <!-- Oppgave 3 -->
+
+<?php
+
+//variabler
+$runde = 0;
+$game = true;
+
+$deltakere = array(
+    "markus" => 0,
+    "jørn" => 0,
+    "frank" => 0,
+    "ole" => 0
+);
+
+annonceContestants($deltakere);
+newRound($deltakere, $runde);
+
+function drawNumber($deltakere){
+    foreach($deltakere as $key => $value){
+        $value+=random_int(1, 50);
+        echo "<p>Deltaker: {$key} | Poeng: {$value} </p>";
+    };
+};
+
+function annonceContestants($deltakere){
+    foreach($deltakere as $key => $value){
+        echo"<p>{$key}</p>";
+    };
+};
+
+function newRound($deltakere, $runde){
+    echo "<h1>Runde: {$runde}</h1>";
+    drawNumber($deltakere);
+    $runde++;
+};
+
+function gameOn($deltakere){
+    while($deltakere > 1){
+        $game = true;
+    }
+}
+
+?>
