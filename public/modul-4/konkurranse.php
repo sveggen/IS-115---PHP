1f508fd781d6bf801706f4920e7e59427d679f8c
Markus Sveggen <marks18@uia.no>
2020-09-22 20:23:07 +0200

    nesten ferdig

--- public/modul-4/konkurranse.php
+++ public/modul-4/konkurranse.php
@@ -1,44 +1,64 @@
 <!-- Oppgave 3 -->
+<html>
+<head>
+<title>Nytt medlem</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/modul-4/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<form method="post" action="">
+<input name="submit" type="submit" value="Send" class="btn btn-dark">
+</form>
 
 <?php
 
-//variabler
-$runde = 1;
-$game = true;
-
-$deltakere = array(
-    "markus" => 0,
-    "jørn" => 0,
-    "frank" => 0,
-    "ole" => 0
-);
-
-annonceContestants($deltakere);
-newRound($deltakere, $runde);
-
-function drawNumber($deltakere){
-    foreach($deltakere as $key => $value){
-        $value+=random_int(1, 50);
-        echo "<p>Deltaker: {$key} | Poeng: {$value} </p>";
-    };
-};
-
-function annonceContestants($deltakere){
-    foreach($deltakere as $key => $value){
-        echo"<p>{$key}</p>";
-    };
-};
-
-function newRound($deltakere, $runde){
-    echo "<h1>Runde: {$runde}</h1>";
-    drawNumber($deltakere);
-    $runde++;
-};
-
-function gameOn($deltakere){
-    while($deltakere > 1){
-        $game = true;
-    }
+$game = false;
+if (isset($_POST['submit'])){
+    $game = true;
 }
 
+
+$deltakere = array("Frank" => 0, "Hans" => 0, "Oliver" => 0, "Hobbe" => 0, "Franker" => 0, "Mona" => 0);
+$runde = 0;
+while (count($deltakere) >= 2 && $game == true){
+    echo "<h2>Runde: " . ++$runde . "\n</h2>";
+
+    # Trekke tall
+    foreach($deltakere as $key => $value){
+        $deltakere[$key] = random_int(1, 50);
+        echo "<p> $key fikk poengsummen $deltakere[$key] \n</p>";
+    }
+
+    # Returner deltaker(e) med lavest poengsum
+    $laveste = array_keys($deltakere, min($deltakere));
+    
+    # Anonnsere flere vinnere
+    if (count(array_unique($deltakere)) == 1){
+        echo "<h3><u>Deltaker " . implode(" og ", array_keys($deltakere)) . " vant</h3></u>";
+break;
+}
+    #Fjern deltaker(e)
+    foreach($laveste as $item){
+            unset($deltakere[$item]);
+            echo "<p>Deltaker " . $item . " ble fjernet</p>\n";
+        }
+
+    # Anonnsere vinner
+    if (count(array_unique($deltakere)) == 1){
+        if (count($deltakere) == 1){
+            echo "<h3><u>Deltaker " . implode(" og ", array_keys($deltakere)) . " vant</h3></u>";
+        break;
+            }
+        }
+    }
+
+
 ?>
+    </body>
+</html>
--- public/modul-4/nyttmedlem.php
+++ public/modul-4/nyttmedlem.php
@@ -1,5 +1,83 @@
 <!-- Oppgave 4 -->
 
-<?php
+<html>
+<head>
+<title>Nytt medlem</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/modul-4/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+  
+<h1>Registrere nytt medlem</h1>
 
+<form action="" method="get">
+<div class="form-group">
+<div class="col-xs-4">
+<p>Fornavn: <input type="text" class="form-control" name="fornavn" />
+Etternavn: <input type="text" class="form-control" name="etternavn" />
+Epost: </br><input type="email" class="form-control" name="epost" />
+Mobilnummer: </br><input type="tel" class="form-control" name="mobilnummer" pattern="[0-9]{8}" />
+Fødselsdato: </br><input type="date" class="form-control" name="dato" />
+<label class="radio-inline"><input type="radio" name="optradio" checked>Mann</label>
+<label class="radio-inline"><input type="radio" name="optradio">Dame</label>
+<label class="radio-inline"><input type="radio" name="optradio">Annet</label>
+Gateadresse: <input type="text" class="form-control" name="gateadresse" />
+Postnummer: <input type="text" class="form-control" name="postnummer" pattern="[0-9]{4}" required/>
+Poststed: <input type="text" class="form-control" name="poststed" />
+
+
+Interesser:
+
+
+Kursaktiviteter:
+
+Roller:
+
+    <input type="submit" class="btn btn-primary" name="knapp" value="Submit"/>
+</div>
+</div>
+</div>
+</p>
+
+
+<?php
+echo "<p> ". $_GET['fornavn'] ." </p>";
+
+$medlem = array();
+$ikkeutfylt = array();
+
+if (isset($_POST['fornavn'])) {
+  array_push($medlem, $_POST['fornavn']);
+} else{
+  array_push($ikkeutfylt, $verdi="Fornavn");
+}
+if (isset($_POST['etternavn'])) {
+  array_push($medlem, $_POST['etternavn']);
+} else{
+  array_push($ikkeutfylt, $verdi="Etternavn");
+}
+if (isset($_POST['telefonnummer'])) {
+  array_push($array, $_POST['telefonnummer']);
+} else{
+  array_push($ikkeutfylt, $verdi="Telefonnummer");
+}
+
+if(isset($_POST['submit']) && !empty($ikkeutfylt)){
+  echo "Følgende felt mangler verdi: " .  implode(", ", $array);
+}
 ?>
+
+
+
+</form>
+
+<div class="card">
+  <div class="card-body">Basic card</div>
+</div>
+</body>
+</html>
